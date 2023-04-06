b51f9002cde988bf9e4cbc646e1486c1802a9378
PhantomOz <faniogor@gmail.com>
2023-04-06 21:37:11 +0100

    This commit will fix receive1

--- src/models/transaction.ts
+++ src/models/transaction.ts
@@ -152,7 +152,7 @@ export default class TransactionStore {
         await TransactionModel.create({
           ...trnx,
           type: "debit",
-          WID: userTo?.[0]?.address || "BLock",
+          WID: trnx?.walletId,
           userFrom: userFrom?.[0]?.userId || {
             _id: "6423b4bfbe63e9d1b99757ae",
           },
